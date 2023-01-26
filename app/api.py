61dd2a926ecc952bb69f7ab4c13f322d16d36f40
tzerok <tae9304@gmail.com>
2023-01-26 21:41:43 +0100

    updated deployment yaml & app pymongo impl with clusterip

--- app/api.py
+++ app/api.py
@@ -5,35 +5,38 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/<DBNAME>"
-# db = PyMongo(app).db
+app.config["MONGO_URI"] = "mongodb://10.8.129.87:27017/snspost"
+db_client = PyMongo(app)
+db = db_client.db
 
-posts = [
-    {
-        'user': u'Tim Berner',
-        'msg': u'La Divina Commeida'
-    },
-    {
-        'user': u'Tim Berner',
-        'msg': u'Ernest Hemingway'
-    },
-    {
-        'user': u'Tim Berner',
-        'msg': u'Ernest Hemingway'
-    }
-]
+# posts = [
+#     {
+#         'user': u'Tim Berner',
+#         'msg': u'La Divina Commeida'
+#     },
+#     {
+#         'user': u'Tim Berner',
+#         'msg': u'Ernest Hemingway'
+#     },
+#     {
+#         'user': u'Tim Berner',
+#         'msg': u'Ernest Hemingway'
+#     }
+# ]
 
-@app.route('/', methods=['GET'])
+@app.route('/posts', methods=['GET'])
 def get_posts():
-    output = list()
-    # posts = db.
-    return jsonify({'posts': posts})
+    # output = list()
+    posts = db.posts.find()
+    # return jsonify({'posts': posts})
+    return jsonify([post for post in posts])
 
 
-@app.route('/postings', methods=['POST'])
+@app.route('/posts', methods=['POST'])
 def add_post():
-    posts.append(json.loads(request.data))
-    return jsonify({'posts': posts})
+    # posts.append(json.loads(request.data))
+    db.posts.insert_one(json.loads(request.data))
+    return jsonify({'HTTP Code': "200"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
